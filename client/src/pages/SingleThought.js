8adb3cc602f004a09ceaf8f60f985ae6f7ac11ad
christopherstrolia-davis <chris.strolia-davis@swarmsolutions.com>
2022-08-25 19:21:38 -0400

    #3 - csd - Update initial content + add apollo/client

---
+++ client/src/pages/SingleThought.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <div className="card mb-3">
+        <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            Username
+          </span>{' '}
+          thought on createdAt
+        </p>
+        <div className="card-body">
+          <p>Thought Text</p>
+        </div>
+      </div>
+    </div>
+  );
+};
