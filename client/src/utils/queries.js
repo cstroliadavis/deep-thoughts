c6379acfdac2b235df8cd0760a7ff367317f673f
christopherstrolia-davis <chris.strolia-davis@swarmsolutions.com>
2022-08-25 21:46:21 -0400

    #4 - csd - Set up the SingleThought page

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,23 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_THOUGHT = gql`
+  query thought($id: ID!) {
+    thought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
 export const QUERY_THOUGHTS = gql`
   query thoughts($username: String) {
     thoughts(username: $username) {
@@ -17,3 +35,4 @@ export const QUERY_THOUGHTS = gql`
     }
   }
 `;
+
