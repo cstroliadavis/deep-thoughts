20c37efda82223138ed2d8eb1032a9005046c27d
christopherstrolia-davis <chris.strolia-davis@swarmsolutions.com>
2022-08-27 10:16:33 -0400

    #6 - csd - Added the Add Thought Mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,6 +14,21 @@ export const ADD_FRIEND = gql`
   }
 `;
 
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
